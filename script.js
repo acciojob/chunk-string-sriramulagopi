0075f2d888d324147b037004a6489ff9de07dad3
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-16 19:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function stringChop(str, size) {
   // your code here
+	let count = 0;
+  let count1 = 0;
+	let array = [];
+	let str1 = "";
+	for (let i=0;i<str.length;i++){
+    str1+=str[i];
+		count++;
+		if (size==count){
+			array.push(str1);
+      count=0;
+      count1++;
+			str1="";
+		}
+	}
+  str1="";
+  for (let i=(count1*size);i<str.length;i++){
+    str1+=str[i];
+  }
+  array.push(str1);
+  if(str.length<size){
+	  return str;
+  }
+  return array;
 }
 
 // Do not change the code below
